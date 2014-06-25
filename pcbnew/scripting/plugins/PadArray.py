d39b2702a511f9af98bfff304b450e30bbeed28a
John Beard <johnb@codexdigital.com>
2014-06-25 22:53:52 +0100

    Overhaul of footprint wizard drawing aids

--- pcbnew/scripting/plugins/PadArray.py
+++ pcbnew/scripting/plugins/PadArray.py
@@ -1,5 +1,26 @@
+#  PadArray.py
+#
+#  Copyright 2014 john <john@johndev>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
 
 import pcbnew
+
 
 class PadMaker:
     """
@@ -9,7 +30,7 @@ class PadMaker:
     def __init__(self, module):
         self.module = module
 
-    def THPad(self, w, l, drill, shape = pcbnew.PAD_OVAL):
+    def THPad(self, w, l, drill, shape=pcbnew.PAD_OVAL):
         pad = pcbnew.D_PAD(self.module)
 
         pad.SetSize(pcbnew.wxSize(l, w))
@@ -22,128 +43,125 @@ class PadMaker:
 
         return pad
 
-    def SMDPad(self, w, l, shape = pcbnew.PAD_RECT):
+    def SMDPad(self, w, l, shape=pcbnew.PAD_RECT):
         pad = pcbnew.D_PAD(self.module)
         pad.SetSize(pcbnew.wxSize(l, w))
 
         pad.SetShape(shape)
 
         pad.SetAttribute(pcbnew.PAD_SMD)
-        pad.SetLayerMask(pcbnew.PAD_CONN_DEFAULT_LAYERS)
+        pad.SetLayerMask(pcbnew.PAD_SMD_DEFAULT_LAYERS)
 
         return pad
 
     def SMTRoundPad(self, size):
-        pad = self.SMDPad(size, size, shape = pcbnew.PAD_CIRCLE)
+        pad = self.SMDPad(size, size, shape=pcbnew.PAD_CIRCLE)
         return pad
+
 
 class PadArray:
 
-    def __init__(self):
-        self.firstPad = 1;
+    def __init__(self, pinNames=None):
+        self.firstPadNum = 1
+        self.pinNames = pinNames
 
     def SetFirstPadInArray(self, fpNum):
-        self.firstPad = fpNum
+        self.firstPadNum = fpNum
 
     # HACK! pad should one day have its own clone method
-    def ClonePad(self):
+    def ClonePad(self, toClone):
 
-        pad = pcbnew.D_PAD(self.pad.GetParent())
+        pad = pcbnew.D_PAD(toClone.GetParent())
 
-        pad.SetSize(self.pad.GetSize())
-        pad.SetShape(self.pad.GetShape())
-        pad.SetAttribute(self.pad.GetAttribute())
-        pad.SetLayerMask(self.pad.GetLayerMask())
-        pad.SetDrillSize(self.pad.GetDrillSize())
+        pad.SetSize(toClone.GetSize())
+        pad.SetShape(toClone.GetShape())
+        pad.SetAttribute(toClone.GetAttribute())
+        pad.SetLayerMask(toClone.GetLayerMask())
+        pad.SetDrillSize(toClone.GetDrillSize())
 
         return pad
 
     def AddPad(self, pad):
         self.pad.GetParent().Add(pad)
 
+
 class PadGridArray(PadArray):
 
-    def __init__(self, pad, nx, ny, px, py, pin1Pos):
+    def __init__(self, pad, nx, ny, px, py, centre=pcbnew.wxPoint(0, 0),
+                 pinNames=None, firstPad=None):
+        PadArray.__init__(self, pinNames)
         # this pad is more of a "context", we will use it as a source of
         # pad data, but not actually add it
         self.pad = pad
+        self.firstPad = firstPad
         self.nx = int(nx)
         self.ny = int(ny)
         self.px = px
         self.py = py
-        self.pin1Pos = pin1Pos
+        self.centre = centre
 
     # handy utility function 1 - A, 2 - B, 26 - AA, etc
     # aIndex = 0 for 0 - A
-    # alphabet = set of allowable chars if not A-Z, eg ABCDEFGHJKLMNPRTUVWY for BGA
-    def AlphaNameFromNumber(self, n, aIndex = 1, alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+    # alphabet = set of allowable chars if not A-Z,
+    #            eg ABCDEFGHJKLMNPRTUVWY for BGA
+    def AlphaNameFromNumber(self, n, aIndex=1,
+                            alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
 
         div, mod = divmod(n - aIndex, len(alphabet))
         alpha = alphabet[mod]
 
         if div > 0:
-            return self.AlphaNameFromNumber(div, aIndex, alphabet) + alpha;
+            return self.AlphaNameFromNumber(div, aIndex, alphabet) + alpha
 
-        return alpha;
+        return alpha
 
     # right to left, top to bottom
     def NamingFunction(self, x, y):
-        return self.firstPad + (self.nx * y + x)
+        return self.firstPadNum + (self.nx * y + x)
 
     #relocate the pad and add it as many times as we need
-    def AddPadsToModule(self):
+    def AddPadsToModule(self, dc):
+
+        pin1posX = self.centre.x - self.px * (self.nx - 1) / 2
+        pin1posY = self.centre.y - self.py * (self.ny - 1) / 2
 
         for x in range(0, self.nx):
+
+            posX = pin1posX + (x * self.px)
+
             for y in range(self.ny):
-                posX = self.pin1Pos.x + (self.px * x)
-                posY = self.pin1Pos.y + (self.py * y)
+                posY = pin1posY + (self.py * y)
 
-                pos = pcbnew.wxPoint(posX, posY)
+                pos = dc.TransformPoint(posX, posY)
 
-                # THIS DOESN'T WORK yet!
-                #pad = self.pad.Clone()
-                pad = self.ClonePad()
+                if (self.firstPad and x == 0 and y == 0):
+                    pad = self.firstPad
+                else:
+                    pad = self.pad
+
+                # THIS DOESN'T WORK YET!
+                # pad = self.pad.Clone()
+                pad = self.ClonePad(pad)
 
                 pad.SetPos0(pos)
                 pad.SetPosition(pos)
 
-                pad.SetPadName(str(self.NamingFunction(x,y)))
+                if self.pinNames is None:
+                    pad.SetPadName(str(self.NamingFunction(x, y)))
+                else:
+                    pad.SetPadName(self.pinNames)
 
                 self.AddPad(pad)
 
+
 class PadLineArray(PadGridArray):
 
-    def __init__(self, pad, n, pitch, isVertical, pin1Pos):
+    def __init__(self, pad, n, pitch, isVertical,
+                 centre=pcbnew.wxPoint(0, 0), pinNames=None, firstPad=None):
 
         if isVertical:
-            PadGridArray.__init__(self, pad, 1, n, 0, pitch, pin1Pos)
+            PadGridArray.__init__(self, pad, 1, n, 0, pitch, centre,
+                                  pinNames, firstPad)
         else:
-            PadGridArray.__init__(self, pad, n, 1, pitch, 0, pin1Pos)
-
-class RectPadArray(PadArray):
-
-    def __init__(self, nx, ny, pitch, xpitch, ypitch, pin1Pos):
-
-        #left row
-        pin1Pos = pcbnew.wxPoint(-h_pitch / 2, -row_len / 2)
-        array = PadLineArray(h_pad, pads_per_row, pad_pitch, True, pin1Pos)
-        array.SetFirstPadInArray(1)
-        array.AddPadsToModule()
-
-        #bottom row
-        pin1Pos = pcbnew.wxPoint(-row_len / 2, v_pitch / 2)
-        array = PA.PadLineArray(v_pad, pads_per_row, pad_pitch, False, pin1Pos)
-        array.SetFirstPadInArray(pads_per_row + 1)
-        array.AddPadsToModule()
-
-        #right row
-        pin1Pos = pcbnew.wxPoint(h_pitch / 2, row_len / 2)
-        array = PadLineArray(h_pad, pads_per_row, -pad_pitch, True, pin1Pos)
-        array.SetFirstPadInArray(2*pads_per_row + 1)
-        array.AddPadsToModule()
-
-        #top row
-        pin1Pos = pcbnew.wxPoint(row_len / 2, -v_pitch / 2)
-        array = PadLineArray(v_pad, pads_per_row, -pad_pitch, False, pin1Pos)
-        array.SetFirstPadInArray(3*pads_per_row + 1)
-        array.AddPadsToModule()
+            PadGridArray.__init__(self, pad, n, 1, pitch, 0, centre,
+                                  pinNames, firstPad)
